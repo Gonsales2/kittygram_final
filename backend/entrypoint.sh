c3157549af82eac860c76b736b025fdf4b1e8f93
elis <chudinastyc@gmail.com>
2026-04-21 20:19:05 +0400

    CI/CD workflow

---
+++ backend/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Waiting for PostgreSQL..."
+while ! nc -z $DB_HOST $DB_PORT; do
+  sleep 1
+done
+echo "PostgreSQL started"
+
+python manage.py migrate --noinput
+
+python manage.py collectstatic --noinput
+
+gunicorn --bind 0.0.0.0:8000 kittygram_backend.wsgi
